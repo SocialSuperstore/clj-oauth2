4f18747ad830fbc65d3b4ecc098f8471cd5ff59c
James Henderson <james@jarohen.me.uk>
2015-03-09 14:11:33 +0000

    Bumping version and adding repo s.t. we can release

--- project.clj
+++ project.clj
@@ -1,7 +1,7 @@
 (def dev-dependencies
   '[[ring "1.1.5"]])
 
-(defproject mavericklou/clj-oauth2 "0.5.2"
+(defproject social.forks/clj-oauth2 "0.5.3-socialsuperstore-SNAPSHOT"
   :description "clj-http and ring middlewares for OAuth 2.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [cheshire "5.0.2"]
@@ -11,6 +11,12 @@
   :exclusions   [org.clojure/clojure-contrib]
   :dev-dependencies ~dev-dependencies
   :profiles {:dev {:dependencies ~dev-dependencies}}
-  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"}
+
+  :repositories {"stuartsierra-releases" "http://stuartsierra.com/maven2"
+                 "releases" {:url "https://larder.socialsuperstore.com"
+                             :creds :gpg}}
+
   :aot [clj-oauth2.OAuth2Exception
-        clj-oauth2.OAuth2StateMismatchException])
+        clj-oauth2.OAuth2StateMismatchException]
+
+  :aliases {"release" ["deploy" "releases"]})
